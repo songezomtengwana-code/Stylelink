d2e27dc8b50e42c68764dc161bab08cc0fdae5c3
songezo_mtengwana <songezomtengwana@gmail.com>
2024-04-14 21:56:30 +0200

    custom composable components

---
+++ app/src/main/java/com/ekasi/studios/stylelink/base/components/LoadingDialog.kt
@@ -0,0 +1,72 @@
+package com.ekasi.studios.stylelink.base.components
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.ekasi.studios.stylelink.ui.theme.StylelinkTheme
+import com.ekasi.studios.stylelink.ui.theme.mediumSize
+
+@Composable
+fun LoadingDialog() {
+    var showDialog by mutableStateOf(true)
+    Dialog(
+        onDismissRequest = { showDialog = false },
+        DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
+    ) {
+        Card(
+            modifier = Modifier
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.background
+            ),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Text(text = "Loading...", color = MaterialTheme.colorScheme.primary)
+                Spacer(modifier = Modifier.height(8.dp))
+                LinearProgressIndicator(
+                    modifier = Modifier.width(mediumSize),
+                    color = MaterialTheme.colorScheme.secondary,
+                    trackColor = MaterialTheme.colorScheme.primary,
+                )
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun LoadingDialogPreview() {
+    StylelinkTheme {
+        LoadingDialog()
+    }
+}
