d8d8cde621a72060b6a1c1478f0c14cf50ac1caa
songezo_mtengwana <songezomtengwana@gmail.com>
2024-06-02 15:35:01 +0200

    updated screen values and path names

--- app/src/main/java/com/ekasi/studios/stylelink/navigation/Screen.kt
+++ app/src/main/java/com/ekasi/studios/stylelink/navigation/Screen.kt
@@ -2,11 +2,11 @@ package com.ekasi.studios.stylelink.navigation
 
 sealed class Screen(val route: String) {
     data object Splash : Screen("splash_screen")
-    data object Main : Screen("main_screen")
+    data object Home : Screen("home")
     data object Register : Screen("register_screen")
     data object Login : Screen("login_screen")
     data object Signup : Screen("signup_screen")
-    data object Search : Screen("search_screen")
+    data object Discover : Screen("discover")
     data object StoreProfile : Screen("store/{storeId}")
 //    object Home : Screen("main_screen")
 }
