c0f1bebd489568db5d7fed2e2b755f0f723b4744
songezo_mtengwana <songezomtengwana@gmail.com>
2024-06-06 16:11:18 +0200

    deleted redundant service (addition)

--- app/src/main/java/com/ekasi/studios/stylelink/data/repository/SupabaseAuthRepository.kt
+++ app/src/main/java/com/ekasi/studios/stylelink/data/repository/SupabaseAuthRepository.kt
@@ -1,40 +1,7 @@
 package com.ekasi.studios.stylelink.data.repository
 
 import android.content.Context
-import android.util.Log
-import com.ekasi.studios.stylelink.utils.api.SupabaseClient
-import io.github.jan.supabase.gotrue.auth
-import io.github.jan.supabase.gotrue.providers.builtin.Email
-import io.github.jan.supabase.gotrue.user.UserInfo
 
 class SupabaseAuthRepository(context: Context) {
-    val supabaseClient = SupabaseClient()
 
-    suspend fun register(email: String, password: String): UserInfo? {
-        val user = supabaseClient.supabase.auth.signUpWith(Email) {
-            email
-            password
-        }
-
-        if (user !== null) {
-            Log.v("supabaseSignUp", "response: $user")
-            return user
-        } else {
-            return null;
-        }
-    }
-
-    suspend fun login(email: String, password: String): Unit? {
-        val user = supabaseClient.supabase.auth.signInWith(Email) {
-            email
-            password
-        }
-
-        if (user !== null) {
-            Log.v("supabaseSignUp", "response: $user")
-            return user
-        } else {
-            return null;
-        }
-    }
 }
